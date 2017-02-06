c5e6e05dd0672b894dbc4ffecaa2aaeef3ce4329
antonybh <aberrospihuerta@gmail.com>
2017-02-06 18:04:42 -0500

    ChangeString.php

---
+++ ChangeString.php
@@ -0,0 +1,14 @@
+# EjerciciosPHP
+<?php 
+class ChangeString {
+    function build(){}
+    }
+$alfabeto[0] = "a";
+$alfabeto[1] = "b";
+$alfabeto[2] = "c";
+$alfabeto[3] = "d";
+$alfabeto[4] = "e";
+for($i=0+1;$i<count($alfabeto);$i++) {
+echo $alfabeto[$i].'';
+    }
+?>
